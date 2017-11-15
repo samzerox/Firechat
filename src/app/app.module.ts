0381c9a4668d6f3e968fac9d3d0eee263631465c
samzerox <sam.zerox88@gmail.com>
2017-11-15 13:00:12 -0600

    Aplicacion terminada y Actualizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+//Componentes
+import { ChatComponent } from './components/chat/chat.component';
+import { LoginComponent } from './components/login/login.component';
+
+//Servicios
+import { ChatService } from './providers/chat.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    ChatService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDbNJbgVebh5Euz6G9xCPsV6oRfjUD0pf4",
+    authDomain: "firechat-447fe.firebaseapp.com",
+    databaseURL: "https://firechat-447fe.firebaseio.com",
+    projectId: "firechat-447fe",
+    storageBucket: "firechat-447fe.appspot.com",
+    messagingSenderId: "766456091519"
+  }
 };
